b5708eddd3b1793c6b8d5ba8aea0d233b0bb6e40
code3743 <jota.lopez@correounivalle.edu.co>
2024-06-17 08:48:30 -0500

    feat: Refactor utils.rkt module and add missing function documentation

--- utils.rkt
+++ utils.rkt
@@ -1,8 +1,16 @@
+;; FILEPATH: ../proyecto_flp/utils.rkt
 #lang racket
+
+;; Provides the following functions:
+;; - to-decimal: Converts a number from a given base to decimal.
+;; - from-decimal: Converts a decimal number to a given base.
+;; - convert-string: Converts a string to a number or symbol.
+;; - operation-numerical: Performs a numerical operation on two numbers.
+;; - array-slice: Extracts a slice from an array.
 
 (provide to-decimal from-decimal convert-string operation-numerical array-slice)
 
-
+;; Converts a number from a given base to decimal.
 (define (to-decimal num base)
   (cond
     [(eq? base 'd) num]
@@ -12,7 +20,7 @@
     )
 )
 
-
+;; Converts a decimal number to a given base.
 (define (from-decimal num base)
   (convert-string  (prefix-base base (cond
     [(eq? base 'd) (number->string num)]
@@ -22,15 +30,17 @@
     )))
 )
 
-
+;; Checks if a string contains letters.
 (define (string-contains-letters? str)
   (regexp-match? #rx"[A-Za-z]" str))
 
+;; Converts a string to a number or symbol.
 (define (convert-string str)
   (if (string-contains-letters? str)
       (string->symbol str)
       (string->number str)))
 
+;; Helper function to add the prefix for a given base.
 (define _prefix-base (lambda (base)
   (cond
     [(eq? base 'd) ""]
@@ -41,20 +51,22 @@
   )
 )
 
+;; Adds the prefix for a given base to a number.
 (define prefix-base (lambda (base num)
-(if (string-contains? num "-")
-  (string-append "-" (_prefix-base base) (string-replace num "-" ""))
-  (string-append (_prefix-base base) num)
-)
-)  
+  (if (string-contains? num "-")
+    (string-append "-" (_prefix-base base) (string-replace num "-" ""))
+    (string-append (_prefix-base base) num)
+  ))  
 )
 
+;; Converts a value to a string representation.
 (define to-string (lambda (x) (cond
   [(string? x) x]
   [(symbol? x) (symbol->string x)]
   [(number? x) (number->string x)]
   )))
 
+;; Extracts the base from a string representation of a number.
 (define extract-base (lambda (str)
   (cond
     [(string-contains? str "b") 'b]
@@ -64,6 +76,7 @@
     ))
 )
 
+;; Performs a numerical operation on two numbers.
 (define operation-numerical 
   (lambda (op num1 num2 boolean?)
     (let ([base (extract-base (to-string num1))]
@@ -78,6 +91,7 @@
   )
 )
 
+;; Extracts a slice from an array.
 (define array-slice
   (lambda (v start end)
     (let (
