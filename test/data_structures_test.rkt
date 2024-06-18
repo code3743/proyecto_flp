6c21c9d90a0a5a393e2ef14ccc96daa956de5437
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2024-06-18 09:36:39 -0500

    Add data_structures_test.rkt for the evaluation of data structures

---
+++ test/data_structures_test.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+(require rackunit)
+(require "../main.rkt")
+
+(define exp1 (scan&parse
+              "struct perro {nombre edad color}
+              let
+                t = new perro (\"lucas\", 10, \"verde\")
+                in get t.nombre"
+                                )
+  )
+
+(check-equal? (eval-program exp1) "lucas" "Test 1 failed")
+
+(define exp2 (scan&parse
+              "struct perro {nombre edad color}
+              let
+                t = new perro (\"lucas\", 10, \"verde\")
+                in begin
+                    set-struct t.nombre = \"pepe\";
+                    get t.nombre
+                  end"
+                                )
+  )
+
+(check-equal? (eval-program exp2) "pepe" "Test 2 failed")
